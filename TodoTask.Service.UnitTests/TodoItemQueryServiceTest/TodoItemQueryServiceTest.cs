5cb4c7984dbc12367a7040e9bae54626a470840b
serhattural <serhattural12@gmail.com>
2022-11-04 18:58:13 +0300

    New test methods added

--- TodoTask.Service.UnitTests/TodoItemQueryServiceTest/TodoItemQueryServiceTest.cs
+++ TodoTask.Service.UnitTests/TodoItemQueryServiceTest/TodoItemQueryServiceTest.cs
@@ -35,8 +35,8 @@ namespace TodoTask.Service.UnitTests.TodoItemQueryServiceTest
 
             //Assert
             Assert.Equal(2, result.Count);
-            Assert.Contains(result, x => x.Id != emptyDueDateItem1.Id);
-            Assert.Contains(result, x => x.Id != emptyDueDateItem2.Id);
+            Assert.True(result.Exists(x => x.Id == emptyDueDateItem1.Id));
+            Assert.True(result.Exists(x => x.Id == emptyDueDateItem2.Id));
         }
 
         [Fact]
@@ -48,9 +48,7 @@ namespace TodoTask.Service.UnitTests.TodoItemQueryServiceTest
             var pendingItem1 = new TodoItem { Id = 2, Title = "todo3", DueDate = new DateTime(2022, 11, 5), IsComplated = false, };
             var data = new List<TodoItem>
             {
-                dueItem1,
-                dueItem2,
-                pendingItem1
+                dueItem1, dueItem2, pendingItem1
             };
             var mockContext = new Mock<TodoTaskDbContext>();
             mockContext.Setup(x => x.TodoItems).ReturnsDbSet(data);
@@ -63,7 +61,32 @@ namespace TodoTask.Service.UnitTests.TodoItemQueryServiceTest
 
             //Assert
             Assert.Equal(1, result.Count);
-            Assert.Contains(result, x => x.Id == pendingItem1.Id);
+            Assert.True(result.Exists(x => x.Id == pendingItem1.Id));
+        }
+
+        public async void GetPendingTodoItems_WhenTodoItemsHasFutureDueDate_ShouldBeInResultList()
+        {
+            //Arrange
+            var pendingItem1 = new TodoItem { Id = 1, Title = "todo1", DueDate = new DateTime(2022, 11, 6), IsComplated = false, };
+            var pendingItem2 = new TodoItem { Id = 2, Title = "todo2", DueDate = new DateTime(2022, 11, 7), IsComplated = false, };
+            
+            var data = new List<TodoItem>
+            {
+                pendingItem1, pendingItem2
+            };
+            var mockContext = new Mock<TodoTaskDbContext>();
+            mockContext.Setup(x => x.TodoItems).ReturnsDbSet(data);
+            var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+            mockDateTimeProvider.Setup(x => x.Now).Returns(new DateTime(2022, 11, 5));
+            var queryService = new TodoItemQueryService(mockContext.Object, mockDateTimeProvider.Object);
+
+            //Act
+            var result = await queryService.GetPendingTodoItems();
+
+            //Assert
+            Assert.Equal(2, result.Count);
+            Assert.True(result.Exists(x => x.Id == pendingItem1.Id));
+            Assert.True(result.Exists(x => x.Id == pendingItem2.Id));
         }
 
         [Fact]
@@ -115,10 +138,10 @@ namespace TodoTask.Service.UnitTests.TodoItemQueryServiceTest
         public async void GetOverdueTodoItems_WhenTodoItemsHasEmptyDueDate_ShouldNotBeInResultList()
         {
             //Arrange
-            var emptDueDateItem = new TodoItem { Id = 0, Title = "todo1", DueDate = null, IsComplated = true, };
+            var emptyDueDateItem = new TodoItem { Id = 0, Title = "todo1", DueDate = null, IsComplated = true, };
             var data = new List<TodoItem>
             {
-                emptDueDateItem
+                emptyDueDateItem
             };
             var mockContext = new Mock<TodoTaskDbContext>();
             mockContext.Setup(x => x.TodoItems).ReturnsDbSet(data);
@@ -156,7 +179,32 @@ namespace TodoTask.Service.UnitTests.TodoItemQueryServiceTest
 
             //Assert
             Assert.Equal(2, result.Count);
-            Assert.Contains(result, x => x.Id != pendingItem1.Id);
+            Assert.True(result.Exists(x => x.Id == dueItem1.Id));
+            Assert.True(result.Exists(x => x.Id == dueItem2.Id));
+            Assert.False(result.Exists(x => x.Id == pendingItem1.Id));
+        }
+
+        public async void GetOverdueTodoItems_WhenTodoItemsHasFutureDueDate_ShouldNotBeInResultList()
+        {
+            //Arrange
+            var pendingItem1 = new TodoItem { Id = 1, Title = "todo1", DueDate = new DateTime(2022, 11, 6), IsComplated = false, };
+            var pendingItem2 = new TodoItem { Id = 2, Title = "todo2", DueDate = new DateTime(2022, 11, 7), IsComplated = false, };
+
+            var data = new List<TodoItem>
+            {
+                pendingItem1, pendingItem2
+            };
+            var mockContext = new Mock<TodoTaskDbContext>();
+            mockContext.Setup(x => x.TodoItems).ReturnsDbSet(data);
+            var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+            mockDateTimeProvider.Setup(x => x.Now).Returns(new DateTime(2022, 11, 5));
+            var queryService = new TodoItemQueryService(mockContext.Object, mockDateTimeProvider.Object);
+
+            //Act
+            var result = await queryService.GetOverdueTodoItems();
+
+            //Assert
+            Assert.Empty(result);
         }
     }
 }
